fef2c371a00725da17231ed0b8a7a36346070d53
Lei Zhu <lei.zhu1@thoughtworks.com>
2020-03-02 14:51:27 +0800

    refactor: rename file

--- src/test/java/com/gildedrose/TextTestFixture.java
+++ src/test/java/com/gildedrose/TextTestFixture.java
@@ -2,7 +2,7 @@ package com.gildedrose;
 
 import java.util.Arrays;
 
-public class TexttestFixture {
+public class TextTestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
