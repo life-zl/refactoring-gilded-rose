907027b9fb93e0e483a1e7be2f0065bf23d75a1a
Lei Zhu <lei.zhu1@thoughtworks.com>
2020-03-07 16:00:58 +0800

    remove updateQuality to Item.class

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -42,4 +42,50 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    public void updateQuality() {
+        if (name.equals("Aged Brie") || name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (sellIn < 11) {
+                        quality = quality + 1;
+                    }
+
+                    if (sellIn < 6) {
+                        quality = quality + 1;
+                    }
+                }
+            }
+        } else {
+            if (quality > 0) {
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    quality = quality - 1;
+                }
+            }
+        }
+
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+            sellIn = sellIn - 1;
+        }
+
+        if (sellIn < 0) {
+            if (!name.equals("Aged Brie")) {
+                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
 }
